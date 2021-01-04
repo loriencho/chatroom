7015ae1c78f1f4a685fd4150e9e5db3595b85ca1
Galadriel Cho <42721783+galadrielcho@users.noreply.github.com>
2021-01-04 13:37:03 -0500

    setup
    integrated node.js, socket.io
    following socket.io tutorial

---
+++ index.js
@@ -0,0 +1,14 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+})
