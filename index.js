4bcf24ba30ec2e2077f75cf5bdcd44d7fda2311c
loriencho <42721802+loriencho@users.noreply.github.com>
2021-01-04 14:27:17 -0500

    continued work on basic setup

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -7,6 +8,13 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+    socket.on('chat message', (msg) =>{
+        console.log('message: ' + msg);
+    });
+    socket.on('disconnect', () =>{
+        console.log('a user disconnected');
+    }    
+    );
 });
 
 http.listen(3000, () => {
