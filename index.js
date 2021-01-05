960021cbdd59c1cbab278cf3ddbd760fda5d1769
Galadriel Cho <42721783+galadrielcho@users.noreply.github.com>
2021-01-05 13:40:33 -0500

    setup finished
    tutorial done, minor edits

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    io.emit('a user connected');
     socket.on('chat message', (msg) =>{
-        console.log('message: ' + msg);
+        io.emit('chat message', msg);
     });
     socket.on('disconnect', () =>{
         console.log('a user disconnected');
