58b43ff1bfc8772201469cb8a57ee277edbdb011
loriencho <42721802+loriencho@users.noreply.github.com>
2021-01-06 10:04:15 -0500

    added users with nickname functionality

--- index.js
+++ index.js
@@ -3,13 +3,30 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html', __dirname + '/main.js');
 });
 
+users = [];
+
 io.on('connection', (socket) => {
+    let d = new Date();
+    
+    const userid = `${d.getHours()}${d.getMinutes()}${d.getSeconds()}`;
+
     io.emit('a user connected');
-    socket.on('chat message', (msg) =>{
-        io.emit('chat message', msg);
+    socket.on('send chat message', (data) =>{
+        console.log(data);
+        io.emit('receive chat message', data);
+    });
+    socket.on('add user', (username) =>{
+        if(users.indexOf(username) > -1){
+            socket.emit('user exists', username);
+        }
+        else{
+            console.log("User added " + username);
+            users.push(username);
+            socket.emit('user set', username);
+        }
     });
     socket.on('disconnect', () =>{
         console.log('a user disconnected');
